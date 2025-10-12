4a4d7b789e27eb8c00a2c9ed072a7ef88ece1b66
Harbers <hans.harbers@solcon.nl>
2025-10-12 16:56:27 +0200

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -2,9 +2,9 @@ from pydantic import BaseModel, Field, HttpUrl
 
 
 class PsalmVersResponse(BaseModel):
-    psalm: int = Field(..., ge=1, le=150, description="Psalmnummer 1..150")
-    vers: int = Field(..., ge=1, description="Versnummer (1..max)")
-    text: str = Field(..., description="Exacte berijmde tekstregel van psalmboek.nl (1773)")
+    psalm: int = Field(..., ge=1, le=150)
+    vers: int = Field(..., ge=1)
+    text: str
     bron: HttpUrl
 
 
