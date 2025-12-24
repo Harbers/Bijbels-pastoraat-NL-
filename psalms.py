0fc29c0174312a8a21333bf7239f179596cd3061
Harbers <hans.harbers@solcon.nl>
2025-12-24 16:14:58 +0100

    Align API with GPT system prompt guidance

--- psalms.py
+++ psalms.py
@@ -1,18 +1,8 @@
-cd /root/bijbels-pastoraat-app
+from psalm_client import PsalmboekClient, client, get_max_vers, get_vers
 
-cat > psalms.py <<'PY'
-# Schone proxy naar psalm_client
-try:
-    # geval 1: psalm_client exporteert functies
-    from psalm_client import get_vers, get_max_vers
-    class _Client:
-        def get_vers(self, psalm: int, vers: int):
-            return get_vers(psalm, vers)
-        def get_max_vers(self, psalm: int):
-            return get_max_vers(psalm)
-    client = _Client()
-except Exception:
-    # geval 2: psalm_client exporteert een klasse
-    from psalm_client import PsalmClient
-    client = PsalmClient()
-PY
+__all__ = [
+    "PsalmboekClient",
+    "client",
+    "get_max_vers",
+    "get_vers",
+]
